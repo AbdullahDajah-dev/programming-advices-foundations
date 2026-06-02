14c83b778f1489e92f58d976692940cc54412369
Abdallah <Abdallah@ABDULLAH-DAJAH>
2026-06-02 19:00:41 +0300

    Add project files.

---
+++ P1.cpp
@@ -0,0 +1,216 @@
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+   
+    //Homework #1 (My ID card)
+
+    /*cout << endl;
+    cout << "********************\n";
+    cout << "Name: \" Abdullah Al-dajah \"\n";
+    cout << "Age: 27\n";
+    cout << "City: Amman\n";
+    cout << "Country: Jordan\n";
+    cout << "********************\n\n\n";
+
+    cout << "****************\n";
+    cout << "****************\n";
+    cout << "****************\n";
+    cout << "****************\n\n\n\n";
+
+
+    cout << "I love programing\n\n" << "I promise to be the best developer ever\n\n" << "I know it will take so much time to learn it, but i will do my best\n\n" << "Best regards\n" << "Abdullah Al-dajah" << endl << endl << endl;
+
+
+    cout << "*\t*\n";
+    cout << "*\t*\n";
+    cout << "* * * * *\n";
+    cout << "*\t*\n";
+    cout << "*\t*\n";
+    cout << endl;*/
+ 
+        // End ----------
+   
+    //Homework #2 (using litrals)
+
+   /* cout << "-------------------------------------------------------------------------" << endl << endl << endl;
+    cout << "\a"; 
+    cout << "hello sir\\madam, nice to meet u! \n\n";
+    cout << "Im \' Abdullah \' talking to you. \n\n";
+    cout << "abdullah\t abdullah\t abdullah \n\n";
+    cout << "abdullah\t abdullah\t abdullah" << endl << endl;*/
+
+       // End ----------
+   
+    //Homework #3
+        
+        // Part 1 (My ID card with variables)
+    
+    /*string Name = "Abdullah Al - dajah", City = "Amman", Country = "Jordan";
+    float Age = 27, MonthlySalary = 850.5;
+    unsigned char Gender = 'M';
+    bool Married = false;
+    
+    cout << "**************************\n";
+    cout << "Name: " << Name << endl;
+    cout << "Age: " << Age << endl;
+    cout << "Gender: " << Gender << endl;
+    cout << "City: " << City << endl;
+    cout << "Country: " << Country << endl;
+    cout << "Monthly Salary: " << MonthlySalary << endl;
+    cout << "Yearly Salary: " << MonthlySalary * 12 << endl;
+    cout << "Married: " << Married << endl;
+    cout << "**************************";
+    cout << endl << endl << endl;*/
+
+       // Part 2  (sum of 3 numbers using variables)
+
+    /*short num1 = 10, num2 = 20, num3 = 30, sum = num1 + num2 + num3;
+
+   
+    
+    cout << num1 << " +" << endl;
+    cout << num2 << " +" << endl;
+    cout << num3 << endl;
+    cout << "------------------\n";
+    cout << sum << endl;*/
+
+      // Part 3 (Age in 5 years with variables)
+
+   /* int Age2 = 27;
+     
+    cout << "After 5 years you will be " << Age2 + 5 << " years old!\n\n\n";*/
+    
+     //End ------------
+
+    //Homework #4
+     
+         //Part 1 (User ID card)
+     
+    /*string YourName, YourCity, YourCountry, AreYouMarried;
+    unsigned short YourAge;
+    float YourMonthlySalary;
+    unsigned char YourGender;
+
+    cout << "Pls enter your name: ";
+    cin >> YourName;
+    cout << "Pls enter your age: ";
+    cin >> YourAge;
+    cout << "pls Enter your gender(M/F): ";
+    cin >> YourGender;
+    cout << "Where you from? ";
+    cin >> YourCountry;
+    cout << "Pls enter your city: ";
+    cin >> YourCity;
+    cout << "Pls enter your Monthly salary: ";
+    cin >> YourMonthlySalary;
+    cout << "Are you married(YES/NO): ";
+    cin >> AreYouMarried;
+    cout << "\n\n" << "Pls check your info" << "\n\n";
+
+    cout << "Name: " << YourName << "\n";
+    cout << "Age: " << YourAge << "\n";
+    cout << "Gender: " << YourGender << "\n";
+    cout << "Country: " << YourCountry << "\n";
+    cout << "City: " << YourCity << "\n";
+    cout << "Your monthly salary: " << YourMonthlySalary << " and u get: " << YourMonthlySalary * 12 << " A year." << "\n";
+    cout << "Married: " << AreYouMarried << "\n\n";*/
+
+       // Part 2 (User sum of 3 numbers)
+
+    /*float numA, numB, numC;
+
+    cout << "First number: ";
+    cin >> numA;
+    cout << "Second number: ";
+    cin >> numB;
+    cout << "Third number: ";
+    cin >> numC;
+    cout << "\n\nResult is >> " << numA + numB + numC << "\n\n";*/
+
+
+      // Part 3 (User Age after 5 years)
+
+    /*short unsigned _YourAge;
+
+    cout << "How old are you? ";
+    cin >> _YourAge;
+    cout << "\nYou will be " << _YourAge + 5 << " in five years\n\n";*/
+   
+        //End ----------
+
+
+   //Homework #5 (User operations)
+
+      // Part 1
+
+
+   /*short A, B;
+   
+   cout << "Pls enter number 1 (A): ";
+   cin >> A;
+
+   cout << "Pls enter number 2 (B): ";
+   cin >> B;
+
+   cout << A << " + " << B << " = " << A + B << "\n";
+   cout << A << " - " << B << " = " << A - B << "\n";
+   cout << A << " * " << B << " = " << A * B << "\n";
+   cout << A << " / " << B << " = " << (float)A / B << "\n";
+   cout << A << " % " << B << " = " << A % B << "\n";
+   cout << endl << endl;*/
+
+          // End -----------
+
+
+   //Homework #6  
+    
+        /*int A, B;
+
+        cout << "Enter A: ";
+        cin >> A;
+
+        cout << "Enter B: ";
+        cin >> B;
+
+        cout << A << " = " << B << " is " << (A == B) << endl;
+        cout << A << " != " << B << " is " << (A != B) << endl;
+        cout << A << " > " << B << " is " << (A > B) << endl;
+        cout << A << " < " << B << " is " << (A < B) << endl;
+        cout << A << " >= " << B << " is " << (A >= B) << endl;
+        cout << A << " <= " << B << " is " << (A <= B) << endl;*/
+            
+           // End ----------
+
+
+            
+
+
+
+
+
+
+
+            
+
+
+
+            
+
+                
+
+            
+    
+
+
+
+
+
+
+
+    return 0;    
+}
+
+
